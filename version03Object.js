8ae79b6aed01e5ec702fa6bb5ba38a576350b94e
kaphasouvor <khambayphasouvor.student@careerdevs.com>
2019-09-16 12:25:11 -0400

    adding js, css and html files

--- version03Object.js
+++ version03Object.js
@@ -3,3 +3,22 @@ var myComputer = {
   screenSize: '15 inches',
   purchaseYear: 2011
 }
+
+var khammy = {
+  name: 'Khammy',
+  // method is a property that is equal to a function. sayName is a method on the gordan object. 
+  sayName: function(){
+    // an anonymous function because it doesnt have a name.
+    console.log(this.name);
+  }
+}
+
+khammy.sayName();
+
+
+
+
+
+console.log(myComputer.screenSize)
+console.log(myComputer.operatingSystem)
+console.log(myComputer.purchaseYear)
