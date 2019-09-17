d3b7ea5d5a6d6c616fd78654b35817d8e72fc526
kaphasouvor <khambayphasouvor.student@careerdevs.com>
2019-09-17 13:15:51 -0400

    Completed Version08 - getting data from inputs

--- script.js
+++ script.js
@@ -62,16 +62,33 @@ var todoList = {
   }
 };
 
-// We want to get access to the display todos button
-var displayTodosButton = document.getElementById('displayTodosButton');
-
-// We want to run displayTodos method when someone click the display todo button.
-displayTodosButton.addEventListener('click', function(){
-  todoList.displayTodos();
-});
-
-// We want to toggle all when the button is clicked
-var toggleAllButton = document.getElementById('toggleAllButton');
-toggleAllButton.addEventListener('click', function(){
-  todoList.toggleAll();
-});
+var handlers = {
+  displayTodos: function() {
+    todoList.displayTodos();
+  },
+  addTodo: function() {
+    var addTodoTextInput = document.getElementById("addTodoTextInput");
+    todoList.addTodo(addTodoTextInput.value);
+    addTodoTextInput.value = ' ';
+  },
+  changeTodo: function() {
+    var changeTodoPositionInput = document.getElementById("changeTodoPositionInput");
+    var changeTodoTextInput = document.getElementById("changeTodoTextInput");
+    todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
+    changeTodoPositionInput.value = ' ';
+    changeTodoTextInput.value = ' ';
+  },
+  deleteTodo: function() {
+    var deleteTodoPositionInput = document. getElementById("deleteTodoPositionInput");
+    todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
+    deleteTodoPositionInput.value = ' ';
+  },
+  toggleCompleted: function(){
+    var toggleCompletedPositionInput = document.getElementById("toggleCompletedPositionInput");
+    todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+    toggleCompletedPositionInput.value = ' ';
+  },
+  toggleAll: function() {
+    todoList.toggleAll();
+  }
+};
