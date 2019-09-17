01277894aad2c1d3dac210d567d9e71a1f77f39b
kaphasouvor <khambayphasouvor.student@careerdevs.com>
2019-09-17 10:17:43 -0400

    verison05 Loops of Logic

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 var todoList = {
   todos: [],
   displayTodos: function() {
-    console.log('My Todos: ', this.todos);
+    // if this.todos.length ===0
+    if(this.todos.length === 0){
+      console.log('Your todo list is empty!');
+    } else {
+      console.log('My Todos: ');
+      for (var i = 0; i < this.todos.length; i++){
+        if (this.todos[i].completed === true) {
+          // print with(x)
+          console.log('(x)', this.todos[i].todoText);
+        } else {
+          // print with ()
+          console.log('( )',this.todos[i].todoText);
+        }
+      }
+    }
   },
   addTodo: function(todoText){
     this.todos.push({
